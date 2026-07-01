fb69db1507fd984634e68f56e096727dd612d849
Fabrizio Agate <agate.fabrizio83@gmail.com>
2026-07-01 18:12:46 +0100

    fix logger

--- src/ClaimFlow.Intake/Program.cs
+++ src/ClaimFlow.Intake/Program.cs
@@ -1,11 +1,18 @@
 using ClaimFlow.Intake;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Azure.Functions.Worker.OpenTelemetry;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
+
+// Integrate the isolated worker's telemetry with the Functions host pipeline:
+// registers the worker ActivitySource (function/service work -> child spans in
+// the host trace) and coordinates logging so the host stops relaying a duplicate
+// copy of each user log.
+builder.Services.AddOpenTelemetry().UseFunctionsWorkerDefaults();
 
 builder.Services.AddSingleton<IIntakeService, IntakeService>();
 
